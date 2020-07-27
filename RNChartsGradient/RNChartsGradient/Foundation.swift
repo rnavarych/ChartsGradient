a60efe1a201740a1f537d78c4c8fd2fe9f42c9c5
Roman Navarych <r.navarych@softteco.com>
2020-07-27 17:23:25 +0300

    init

---
+++ RNChartsGradient/RNChartsGradient/Foundation.swift
@@ -0,0 +1,9 @@
+//
+//  Foundation.swift
+//  RNChartsGradient
+//
+//  Created by Raman on 7/27/20.
+//  Copyright © 2020 Raman. All rights reserved.
+//
+
+import Foundation
---
+++ RNChartsGradient/RNChartsGradient/RNChartsGradient.h
@@ -0,0 +1,19 @@
+//
+//  RNChartsGradient.h
+//  RNChartsGradient
+//
+//  Created by Raman on 7/27/20.
+//  Copyright © 2020 Raman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNChartsGradient.
+FOUNDATION_EXPORT double RNChartsGradientVersionNumber;
+
+//! Project version string for RNChartsGradient.
+FOUNDATION_EXPORT const unsigned char RNChartsGradientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNChartsGradient/PublicHeader.h>
+
+
