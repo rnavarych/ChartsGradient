a1d98faa6c85a48d85b939380606cc924533bc88
Roman Navarych <r.navarych@softteco.com>
2020-08-07 11:41:58 +0300

    remove crash

--- RNChartsGradient/Source/Charts/Renderers/LineChartRenderer.swift
+++ RNChartsGradient/Source/Charts/Renderers/LineChartRenderer.swift
@@ -81,9 +81,9 @@ open class LineChartRenderer: LineRadarRenderer
 
         case .horizontalBezier:
             drawHorizontalBezier(context: context, dataSet: dataSet)
+            context.restoreGState()
         }
 
-        context.restoreGState()
     }
 
     private func drawLine(
